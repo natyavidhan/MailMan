3b10a7f6a9bee759394aa6ea28ae0904faf53b1f
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-06 19:20:41 +0530

    most ui done also i forgor to commit :skull:

---
+++ main.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("MailMan")
+        self.root.geometry("950x470")
+        self.root.resizable(False, False)
+
+        self.header = ctk.CTkFrame(self.root, corner_radius=10)
+        self.header.place(width=445, height=80, x=25, y=20)
+
+        ctk.CTkLabel(self.header, text="MailMan", text_font=("Acme", 30, "bold")).place(x=10, y=0, width=180, height=80)
+        ctk.CTkButton(self.header, text="New Request", command=self.new_request, corner_radius=10, text_font=("Acme", 18, "bold")).place(x=200, y=15, width=235, height=55)
+
+        self.active_request_frame = ctk.CTkFrame(self.root, corner_radius=10)
+        self.active_request_frame.place(width=445, height=80, x=480, y=20)
+
+        ctk.CTkLabel(self.active_request_frame, text="Active Request", text_font=("Acme", 15, "bold")).place(x=10, y=0, width=180, height=80)
+        self.current_request = ttk.Combobox(self.active_request_frame, values=["Request 1", "Request 2", "Request 3", "Request 4", "Request 5"], state="readonly")
+        self.current_request.place(x=200, y=30, width=235, height=25)
+
+        self.headers_frame = ctk.CTkFrame(self.root, corner_radius=10)
+        self.headers_frame.place(width=445, height=145, x=25, y=135)
+
+        self.body_frame = ctk.CTkFrame(self.root, corner_radius=10)
+        self.body_frame.place(width=445, height=145, x=25, y=290)
+
+        self.options_frame = ctk.CTkFrame(self.root, corner_radius=10)
+        self.options_frame.place(width=440, height=260, x=480, y=175)
+
+        self.url = tk.Entry(self.root, font=("Acme", 12, "bold"))
+        self.url.place(x=480, y=140, width=310, height=30)
+
+        self.request_type = ttk.Combobox(self.root, values=["GET", "POST", "PUT", "DELETE"], state="readonly")
+        self.request_type.place(x=800, y=140, width=120, height=30)
+
+    def new_request(self):
+        pass
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    app = App(root)
+    root.mainloop()
